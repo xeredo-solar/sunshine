60619bceb0ffdc359c85bcbe5d57ab1f9a3a786a
Maciej Krüger <mkg20001@gmail.com>
2020-08-23 12:41:24 +0200

    feat: add nix stuff

---
+++ module.nix
@@ -0,0 +1,66 @@
+{ lib, pkgs, config, ... }:
+
+with lib;
+
+let
+  cfg = config.services.sunshine;
+in
+{
+  options = {
+    services.sunshine = {
+      enable = mkEnableOption "sunshine";
+      extraConfig = mkOption {
+        type = types.lines;
+
+        default = "";
+
+        example = ''
+          gc:
+            onInterval: true # true=daily, false=never or string containing cron interval
+            onLowSpace: 2147483648 # 2gb, 0 to disable, will trigger every minute and check threshold on /nix/store partition
+            keepDays: 7 # how many days to keep old, unused stuff arround for rollbacks
+            maxFree: 0 # _only_ free _up to_ this much space, 0=ignore
+          system:
+            upgrade: # affects nixos-rebuild
+              silentFetch: true # fetch channels silently, true=daily false=never or string in cron interval
+              silentPrepare: true # prefetch already built derivations from cache but don't build anything just yet, will not run on paid networks (e.g. mobile)
+              forceSilentPrepare: false # always run silentPrepare
+              silentApplyForNextBoot: true # build with nixos-rebuild boot so update gets applied on next boot
+              notifyUpdateAvailable: true # make a notification to apply update during current boot
+              rollback:
+                checks:
+                  sanityCheck: true # perform checks while the system is running
+                  grubFlag: false # add settings to grub to auto-rollback
+                allowAuto: critical # never, critical, always - change when auto-rollbacks are done
+          user:
+            upgrade: # affects nix-env
+              silentFetch: true # fetch channels silently, true=daily false=never or string in cron interval
+              silentPrepare: true # prefetch already built derivations from cache but don't build anything just yet, will not run on paid networks (e.g. mobile)
+              forceSilentPrepare: false # always run silentPrepare
+          cooldownMsAfterBoot: 18000000 # 5 min
+        '';
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.sunshine = {
+      description = "System update daemon";
+
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      environment = {
+        SUNSHINE_SYSTEMD = "1";
+      };
+
+      path = [ pkgs.sunshine config.nix.package ];
+
+      script = "${pkgs.sunshine}/bin/sunshine";
+
+      serviceConfig = {
+        Restart = "on-failure";
+      };
+    };
+  };
+}
--- src/scripts.js
+++ src/scripts.js
@@ -3,27 +3,62 @@
 // every function that directly uses a script
 
 const path = require('path')
-const scripts = require('scriptrr')({
-  files: [path.join(__dirname, 'scripts', '*.sh')],
-  showOutput: true,
-  options: {}
-})
+const scriptrr = require('scriptrr')
+const scriptCtx = (options = {}) =>
+  scriptrr({
+    files: [path.join(__dirname, 'scripts', '*.sh')],
+    showOutput: true,
+    options
+  })
+
+const scripts = scriptCtx()
 
 const {
+  read,
   processDryRun,
   withTmp
 } = require('./util')
 
+const users = {}
+
+read('/etc/passwd').split('\n').filter(l => Boolean(l.trim())).map(l => {
+  const [name, pw, uid, gid, comment, home, shell] = l.split(':')
+  return {
+    name,
+    pw: pw === 'x' ? true : pw || false,
+    uid: parseInt(uid, 10),
+    gid: parseInt(gid, 10),
+    comment,
+    home,
+    shell
+  }
+}).filter(({ uid, home, shell }) =>
+  uid >= 1000 && home.startsWith('/home') && !shell.endsWith('nologin')
+).forEach(user => {
+  users[user.name] = scriptCtx({
+    cwd: user.home,
+    uid: user.uid,
+    gid: user.gid,
+    env: Object.assign(Object.assign({}, process.env), {
+      HOME: user.home
+    })
+  })
+})
+
 module.exports = {
+  getUsers () {
+    return Object.keys(users)
+  },
+
   async dryRun (drv) {
     return processDryRun(
       await withTmp(p => scripts.dryRun(drv, p))
     )
   },
 
-  async dryRunNixEnv () {
+  async dryRunNixEnv (user) {
     return processDryRun(
-      await withTmp(p => scripts.dryRunNixEnvUpdate(p))
+      await withTmp(p => users[user].dryRunNixEnvUpdate(p))
     )
   },
 
