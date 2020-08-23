7143ba07563576f4f8d1f9b5d8cffe0848d1e9b4
Maciej Krüger <mkg20001@gmail.com>
2020-08-23 15:27:28 +0200

    fix: opencollective-postinstall bug

--- package.nix
+++ package.nix
@@ -4,4 +4,20 @@ let
     root = ./.;
     nodejs = nodejs-14_x;
   };
-in makeNode { }
+in makeNode {
+  preBuild = ''
+    opName="node_modules/.bin/opencollective-postinstall"
+    opDirs=("$out/$opName" "$out/$opName")
+    (
+      while sleep .1s; do
+        for p in ''${opDirs[*]}; do
+          (echo true > $p) 2>/dev/null || true
+        done
+      done
+    ) & opPid=$!
+  '';
+
+  postDist = ''
+    kill $opPid
+  '';
+}
