d463b0d2a3f9d8d3654f68fcf09660a6797acade
Maciej Krüger <mkg20001@gmail.com>
2020-08-23 15:05:02 +0200

    fix: package

--- package.nix
+++ package.nix
@@ -1,10 +1,6 @@
+{ mkNode }:
 let
-  pkgs = import <nixpkgs> {};
-  nixNodePackage = builtins.fetchGit {
-    url = "git@github.com:mkg20001/nix-node-package";
-    rev = "6ece1177d0c0b8aca1a983744099235f4191d735";
-  };
-  makeNode = import "${nixNodePackage}/nix/default.nix" pkgs {
+  makeNode = mkNode {{
     root = ./.;
     nodejs = pkgs.nodejs-14_x;
   };
